0e16465b39bf83f71321cab83a7a6728d9013da3
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-04-22 09:30:29 -0500

    Created new file creation feature

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,11 +1,88 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the name of the file employee file to process: ");
+            String employeeFileName = input.nextLine();
+
+            System.out.print("Enter the name of the payroll file to create: ");
+            String payrollFileName = input.nextLine();
+
+            Scanner fileScanner = new Scanner(new File(employeeFileName));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(payrollFileName));
+
+            fileScanner.nextLine();
+            if (payrollFileName.endsWith(".csv")) {
+                writer.write("id|name|gross pay\n");
+            } else if (payrollFileName.endsWith(".json")) {
+                writer.write("[\n");
+            }
+
+            boolean first = true;
+            while (fileScanner.hasNextLine()) {
+
+                if (payrollFileName.endsWith(".csv")) {
+                    String line = fileScanner.nextLine();
+
+                    String[] parts = line.split("\\|");
+
+                    String id = parts[0];
+                    String name = parts[1];
+                    double payRate = Double.parseDouble(parts[2]);
+                    double hours = Double.parseDouble(parts[3]);
+
+                    double grossPay = payRate * hours;
+
+                    String output = String.format("%s|%s|%.2f\n", id, name, grossPay);
+
+                    writer.write(output);
+                } else if (payrollFileName.endsWith(".json")) {
+                    String line = fileScanner.nextLine();
+
+                    String[] parts = line.split("\\|");
+
+                    String id = parts[0];
+                    String name = parts[1];
+                    double payRate = Double.parseDouble(parts[2]);
+                    double hours = Double.parseDouble(parts[3]);
+
+                    double grossPay = payRate * hours;
+
+                    if (!first) {
+                        writer.write(",\n");
+                    }
+
+                    String output = String.format(
+                            "     { \"id\": %s, \"name\" : \"%s\", \"grossPay\" : %.2f }",
+                            id, name, grossPay);
+
+                    writer.write(output);
+
+                    first = false;
+                }
+            }
+
+            if (payrollFileName.endsWith(".json")) {
+                writer.write("\n]");
+            }
+
+            writer.close();
+            fileScanner.close();
+        }
+        catch (IOException e) {
+            System.out.println("ERROR: An unexpected error occurred");
+            e.printStackTrace();
+        }
+
+        //showEmployees();
+    }
+
+    public static void showEmployees() {
         try {
             BufferedReader bufReader = new BufferedReader(new FileReader("employees.csv"));
             String input;
